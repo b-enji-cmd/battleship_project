3ae9bd439d020af18b7490e1c7dcb89cff192dae
Dominic Padula <domopadula2192@gmail.com>
2020-10-13 20:52:56 -0600

    add fired upon logic

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,27 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :taken_fire
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = ship
+    @taken_fire = false
+  end
+
+  def empty?
+    if @ship == nil
+      true
+    else false
+    end
+  end
+
+  def place_ship(placed_ship)
+    @ship = placed_ship
+  end
+
+  def fired_upon?
+     @taken_fire
+  end
+
+  def fire_upon
+    @taken_fire = true 
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -7,9 +7,24 @@ class CellTest < Minitest::Test
 
   def setup
     @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
   def test_it_exists
 
     assert_instance_of Cell, @cell
-  end  
+  end
+  def test_it_can_return_empty_state
+    assert_equal true, @cell.empty?
+  end
+  def test_if_ship_is_placed
+    @cell.place_ship(@cruiser)
+    assert_equal @cruiser, @cell.ship
+  end
+  def test_if_fired_upon_returns_state
+    assert_equal false, @cell.fired_upon?
+  end
+  def test_fire_upon_changes_state
+    @cell.fire_upon
+    assert_equal true, @cell.fired_upon?
+  end
 end
