b45d64bbe2917327639c12c868772f39a53ee0f6
Dominic Padula <domopadula2192@gmail.com>
2020-10-13 18:54:46 -0600

    add cell class

---
+++ test/cell_test.rb
@@ -0,0 +1,15 @@
+require './lib/ship'
+require './lib/cell'
+require 'pry'
+require 'minitest/autorun'
+
+class CellTest < Minitest::Test
+
+  def setup
+    @cell = Cell.new("B4")
+  end
+  def test_it_exists
+
+    assert_instance_of Cell, @cell
+  end  
+end
