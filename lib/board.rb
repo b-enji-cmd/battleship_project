f8b14bd2045cf5ac52a49d4dc77018a591f06e37
Ben Fulton <benjamin.fulton99@gmail.com>
2020-10-16 20:56:45 -0600

    Add placement validation

--- lib/board.rb
+++ lib/board.rb
@@ -16,4 +16,69 @@ class Board
      @cells.include?(coord) 
   end
 
+  def valid_length?(ship_object, given_coordinate_arr)
+    ship_object.length == given_coordinate_arr.count
+  end
+
+  def valid_consecutive_numbers?(ship_object, given_coordinate_arr)
+    only_numbers = given_coordinate_arr.map do |coord|
+          coord.chars
+    end
+    
+    valid_numbers = only_numbers.map do |nested_arr|
+        nested_arr[1].to_i
+    end
+    
+    if valid_numbers.uniq.count == 1
+        return true
+    else
+    
+      valid_numbers.each_cons(2).all? {|a , b| b == a+1}
+    
+    end
+
+  end
+
+  def valid_consecutive_letters?(ship_object, given_coordinate_arr)
+    arg_letters =  given_coordinate_arr.map do |coord|
+         coord.chars
+    end
+    arg_letters
+    valid_letters = arg_letters.map do |nested|
+      nested[0]
+    end
+    if valid_letters.uniq.count == 1
+      return true
+    else
+
+     valid_ordinal = valid_letters.map do |letter|
+          letter.ord
+     end
+
+      valid_ordinal.each_cons(2).all? {|a , b| b == a+1}
+    end
+  end
+
+  def valid_consecutive?(ship_object, given_coordinate_arr)
+    valid_consecutive_numbers?(ship_object, given_coordinate_arr) && valid_consecutive_letters?(ship_object, given_coordinate_arr)
+  end
+
+  def valid_placement?(ship_object, given_coordinate_arr)
+    valid_length?(ship_object, given_coordinate_arr) &&
+    valid_consecutive?(ship_object, given_coordinate_arr)&&
+    !valid_diagonal?(ship_object, given_coordinate_arr)
+
+  end
+
+  def valid_diagonal?(ship_object, given_coordinate_arr)
+    arg_letter = []
+    arg_num = []
+    given_coordinate_arr.each do |coord|
+        coord
+        arg_letter << coord[0]
+        arg_num << coord[1].to_i
+    end
+    (arg_letter.uniq.count> 1 ) && (arg_num.uniq.count> 1 )
+  end
+
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,19 +1,22 @@
 require 'pry'
 require './lib/board'
 require './lib/cell'
+require './lib/ship'
 require 'minitest/autorun'
 
 class BoardTest < Minitest::Test
 
   def setup
   @board = Board.new
+  @cruiser = Ship.new("Cruiser", 3)
+  @submarine = Ship.new("Submarine", 2)    
   end
 
   def test_it_exists
-    test_hash = {}
+    #test_hash = {}
     assert_instance_of Board, @board
 
-    assert_equal test_hash, @board.cells
+    #assert_equal test_hash, @board.cells
   end
 
   def test_it_can_populate_board
@@ -28,6 +31,13 @@ class BoardTest < Minitest::Test
     refute @board.valid_coordinate?("A22")
   end
 
-  def test_
+  def test_it_can_validate_placement
+    #binding.pry
+    assert_equal false , @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false , @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false , @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false , @board.valid_placement?(@cruiser, ["A1", "C1", "D1"])
+    assert_equal true , @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
+  end
 
 end
